41a3e1dd253c38b6959f84fea9b686a7c7bd2e8a
sooraj suneesh <soorajsuneesh@gmail.com>
2020-11-26 21:12:39 -0500

    background image

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
 
 import React from 'react';
 import { View,Text } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
-
-
-
-  return ( <View style={{
-    backgroundColor : "#fff",
-    flex: 1,
-    justifyContent:"center",
-    alignItems:"center"
-    
-    
-  }}>
-    <View style={{
-      backgroundColor:"dodgerblue",
-     width: 100,
-     height:100
-    }} />
-      <View style={{
-      backgroundColor:"gold",
-      width: 100,
-      height:100
-    }} />
-      <View style={{
-      backgroundColor:"tomato",
-      width: 100,
-     height:100
-    }} />
-   
-
-    </View>
-   
-
- 
-   
-
-  );
+  return(
+  
+ <WelcomeScreen /> )
 }
